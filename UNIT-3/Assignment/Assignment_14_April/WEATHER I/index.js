d9032da3b4a07d9ff7156cab96f620a36663b8df
sagar pawar <sagarpawar1493@gmail.com>
2022-04-17 08:34:27 +0530

    promises

--- UNIT-3/Assignment/Assignment_14_April/WEATHER I/index.js
+++ UNIT-3/Assignment/Assignment_14_April/WEATHER I/index.js
@@ -18,6 +18,56 @@ function getData() {
     })
 }
 
+function getDataLocation(lat, lon) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=56e1f416b2f4a81d2839535a134a3c9e`
+
+    fetch(url) 
+    .then(function(res) {
+        return res.json()
+    })
+    .then(function(res) {
+        append(res);
+        console.log(res)
+    })
+    .catch(function(err) {
+    console.log(err)
+    })
+}
+
+// function getWeather7Day(lat, lon) {
+//     const url_new = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=56e1f416b2f4a81d2839535a134a3c9e`
+
+//         fetch(url_new) 
+//         .then(function(res) {
+//             return res.json()
+//         })
+//         .then(function(res) {
+//             append(res);
+//             console.log(res)
+//         })
+//         .catch(function(err) {
+//         console.log(err)
+//         })
+// }
+
+// function append7Day(data) {
+//     let main = document.querySelector("#sevenData");
+    
+//     data.map(function(ele) {
+        
+//             let box = document.createElement("div")
+
+//             let temp = document.createElement("p")
+//             temp.innerText = `Temperature : ${Math.round(ele.temp.day -273)} °C`
+
+//             box.append(temp)
+
+//             main.append(box)
+//     })
+// }
+
+
+
 function append(data) {
     let weather = document.getElementById("weather")
     let map = document.getElementById("gmap_canvas")
@@ -28,16 +78,16 @@ function append(data) {
     city.innerText = `${data.name}`
 
     let min = document.createElement("p")
-    min.innerText = `Min Temp :   ${data.main.temp_min}`
+    min.innerText = `Minimum Temperature :   ${Math.round(data.main.temp_min -273)} °C`;
 
     let max = document.createElement("p")
-    max.innerText = `Max Temp :   ${data.main.temp_max}`
+    max.innerText = `Maximum Temperature :   ${Math.round(data.main.temp_max -273)} °C`
 
     let current = document.createElement("p")
-    current.innerText = `Current Temp :   ${data.main.temp}`
+    current.innerText = `Current Temperature :   ${Math.round(data.main.temp -273)} °C`
 
     let humidity = document.createElement("p")
-    humidity .innerText = `Humidity :   ${data.main.humidity}`
+    humidity .innerText = `Humidity :   ${Math.round(data.main.humidity)}`
 
     weather.append(city, min, max, current, humidity)
     map.src = `https://maps.google.com/maps?q=${data.name}&t=&z=13&ie=UTF8&iwloc=&output=embed`
@@ -53,6 +103,10 @@ function geolocation() {
         console.log(`Latitude: ${crd.latitude}`);
         console.log(`Longitude: ${crd.longitude}`);
         console.log(`More or less ${crd.accuracy} meters.`);
+
+        getDataLocation(crd.latitude, crd.longitude)
+
+        getWeather7Day(crd.latitude, crd.longitude)
     }
 }
 
